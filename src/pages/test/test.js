09060b7197a89ea2b1b6e752ceedc44c98fd65f4
jahiker <rojasjahiker@gmail.com>
2023-09-04 22:44:53 -0400

    [add] styles, javascript and astro componets

---
+++ src/pages/test/test.js
@@ -0,0 +1,4 @@
+export const dataPage = {
+    postTitle: "This is a test page",
+    postContent:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores delectus tempora porro, mollitia hic eum quas minima earum ipsa placeat ratione maxime neque fuga dicta id quidem ea libero commodi!"
+}
